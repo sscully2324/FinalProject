64d77708911a639f8e58600fc38c093df142bc07
srscully <srscully@umich.edu>
2022-11-30 15:51:36 -0500

    hello

--- finalp.py
+++ finalp.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 import sqlite3
 import os
 
-#GNEWS
+#GNEWS SETUP
 def get_news_data_gnews(stocksTicker, from_date, to_date, sort, limit):
-    api_key = "e0cb64f718bf6b042c7faa469cc4a7cd"
+    api_key = "e0cb64f718bf6b042c7faa469cc4a7cd" #enter your api key here
     url = "https://gnews.io/api/v4/search?q=" + stocksTicker + "&from=" + from_date + "&to=" + to_date + "&sortby=" + sort + "&max=" + limit + "&token=" + api_key+ "&lang=en" 
     params = {
         "stocksTicker": stocksTicker,
@@ -21,7 +21,7 @@ def get_news_data_gnews(stocksTicker, from_date, to_date, sort, limit):
     response = requests.get(url, params=params)
     data = response.json()
     return data
-#POLYGON
+#POLYGON SETUP
 def get_stock_data_polygon(stocksTicker, multiplier, timespan, from_date, to_date, sort, limit):
     url = "https://api.polygon.io/v2/aggs/ticker/" + stocksTicker + "/range/" + multiplier + "/" + timespan + "/" + from_date + "/" + to_date + "?apiKey=kQ7_G94gpM45HRLw4XrF9a_pW1sRLNqb" #enter your api key here
     params = {
@@ -36,7 +36,7 @@ def get_stock_data_polygon(stocksTicker, multiplier, timespan, from_date, to_dat
     response = requests.get(url, params=params)
     data = response.json()
     return data
-#TWELVEDATA
+#TWELVEDATA SETUP
 def get_current_stock_data(symbol,interval):
     url="https://api.twelvedata.com/time_series"
     params = {
@@ -51,9 +51,7 @@ def get_current_stock_data(symbol,interval):
 
 '''--------------------------------------------------------------------------------------------------------------'''
 
-#SQL
-
-#GNEWS
+#GNEWS SQL
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path + '/' + db_name)
@@ -80,7 +78,7 @@ def print_db_table_news(cur, conn):
     print("Total Articles: " + str(len(rows)))
     print("---------------------------------------------------------")
 
-#POLYGON
+#POLYGON SQL
 def create_table_poly(cur, conn):
     cur.execute("CREATE TABLE IF NOT EXISTS stock (date TEXT, open REAL, high REAL, low REAL, close REAL, volume REAL)")
     conn.commit()
@@ -102,7 +100,7 @@ def print_db_table_poly(cur, conn):
     df.columns = [x[0] for x in cur.description]
     print(df)
 
-#TWELVEDATA
+#TWELVEDATA SQL
 def create_table(cur, conn):
     cur.execute("CREATE TABLE IF NOT EXISTS realtime (date TEXT, open REAL, high REAL, low REAL, close REAL, volume REAL)")
     conn.commit()
@@ -123,34 +121,11 @@ def print_db_table(cur, conn):
     df = pd.DataFrame(rows)
     df.columns = [x[0] for x in cur.description]
     print(df)
-
     
 '''--------------------------------------------------------------------------------------------------------------'''
 
-
-#TWELVEDATA API (current stock data)
-def get_current_stock_data(symbol,interval):
-    url="https://api.twelvedata.com/time_series"
-    params = {
-        "symbol":symbol,
-        "interval":interval,
-        "apikey":"9699f1b5bb0b4a8c9a54b2e112630369"
-    }
-
-    response=requests.get(url,params=params)
-    data=response.json()
-    print(data)
-    return data
-
-#Get time series values, and calculate average for the current day. 
-#Add all of these values to SQL table
-def get_twelvedata_table(data):
-    data['values']
-    pass
-
 def main():
     tickers = ["AAPL", "MSFT"]
-    #Polygon API
     if len(tickers) > 1:
         for ticker in tickers:
             data = get_news_data_gnews(ticker, "2021-05-01", "2022-05-05", "date", "10")
@@ -170,21 +145,37 @@ def main():
             create_table(cur, conn)
             insert_data(cur, conn, data)
             print(ticker)
-            print_poly_table(data)
-            print("---------------------------------------------------------")
-            plot_poly_data(data)
+            print_db_table(cur, conn)
+            conn.close()
+            td = TDClient(apikey="9699f1b5bb0b4a8c9a54b2e112630369") #add your api key here
+            ts = td.time_series(
+                symbol=ticker,
+                outputsize=100,
+                interval="1min",
+            )
+            ts.as_plotly_figure()
+            ts.with_ema().as_plotly_figure().show()
     elif len(tickers) == 1:
-        data = get_stock_data_polygon(tickers[0], "1", "day", "2020-05-05", "2021-05-05", "asc", "100")
-        print(tickers[0])
-        print_poly_table(data)
-        print("---------------------------------------------------------")
-        plot_poly_data(data)
-    else:
-        print("No Tickers")
-    
-    #TwelveData API
-    for ticker in tickers:
-        td = TDClient(apikey="9699f1b5bb0b4a8c9a54b2e112630369")
+        data = get_news_data_gnews(tickers[0], "2021-05-01", "2022-05-05", "date", "10")
+        cur, conn = setUpDatabase(ticker + ".db")
+        create_table_news(cur, conn)
+        insert_data_news(cur, conn, data)
+        print(ticker)
+        print_db_table_news(cur, conn)
+        data = get_stock_data_polygon(tickers[0], "1", "day", "2020-01-05", "2022-05-01", "asc", "100")
+        cur, conn = setUpDatabase(ticker + ".db")
+        create_table_poly(cur, conn)
+        insert_data_poly(cur, conn, data)
+        print(ticker)
+        print_db_table_poly(cur, conn)
+        data = get_current_stock_data(tickers[0], "1min")
+        cur, conn = setUpDatabase(ticker + ".db")
+        create_table(cur, conn)
+        insert_data(cur, conn, data)
+        print(ticker)
+        print_db_table(cur, conn)
+        conn.close()
+        td = TDClient(apikey="9699f1b5bb0b4a8c9a54b2e112630369") #add your api key here
         ts = td.time_series(
             symbol=ticker,
             outputsize=100,
@@ -192,6 +183,8 @@ def main():
         )
         ts.as_plotly_figure()
         ts.with_ema().as_plotly_figure().show()
+    else:
+        print("No Tickers")
 
 if __name__ == "__main__":
     main()
