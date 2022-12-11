377cee2a3e3ed1006d95bd0abbad6f5499b65080
Anabelle Phillips <anphilli@umich.edu>
2022-12-11 16:24:56 -0500

    chagnes

--- twelvedataviz.py
+++ twelvedataviz.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import plotly.express as px
 import plotly.graph_objects as go
 
+#Making First Vizualization with TwelveData API
 #TWELVEDATA SETUP
 def get_current_stock_data(symbol,interval, outputsize, apikey):
     url="https://api.twelvedata.com/time_series" + "?symbol=" + symbol + "&interval=" + interval + "&apikey=" + apikey + "&outputsize=" + outputsize
@@ -25,16 +26,16 @@ def get_current_stock_data(symbol,interval, outputsize, apikey):
 #Print TwelveData Table
 def print_current_table(data):
     df = pd.DataFrame(data['values'])
-    #!!!!!!!!!need to change format of date somehow !!!!!!!!
     df = df.set_index('datetime')
     df.columns = ['Open','High','Low','Close',"Volume"]
+    #calculation from table here
     df['Average Value'] = round((df['High'].astype(float)+df['Low'].astype(float))/2,4)
     #returns dataframe 
     return df
 
 def main():
     stocks = "AAPL"
-    current = get_current_stock_data(stocks, "1min", "25", "f3cdc7c31b684fdf9de786f2f913fa51")
+    current = get_current_stock_data(stocks, "1min", "100", "f3cdc7c31b684fdf9de786f2f913fa51")
     tbl = print_current_table(current)
     print(tbl)
 
@@ -44,7 +45,7 @@ def main():
         xAxis.append(d['datetime'])
         
     # Make VIZ 1 from TwelveData API 
-    fig = px.line(tbl, x=xAxis, y="Average Value", color= "Average Value", title="Average " + current['meta']['symbol'] + " Stock Value per Current Minute")
+    fig = px.line(tbl, x=xAxis, y="Average Value", title="Average " + current['meta']['symbol'] + " Stock Value per Current Minute").update_layout(xaxis_title="Current Time (Military)")
     fig.update_traces(line_color='purple')
     fig.update_layout(
     font_family="Courier New",
